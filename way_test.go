0eb5e157bde38c77d3a2e932c4ef04923f75df7e
Ole Bulbuk <ole@ambiweb.de>
2017-05-23 14:43:58 +0200

    Added dummy routes for all benchmarks and b.ResetTimer().

--- way_test.go
+++ way_test.go
@@ -184,6 +184,57 @@ type benchmarkData struct {
 // only needed to prevent the compiler from optimizing the code too much
 var benchmarkCounter int
 
+// dummy routes used to fill up the routes table and make the benchmark more interesting
+var benchmarkDummyRoutes = []string{
+	// don't match at all:
+	"/nomatch/one",
+	"/nomatch/two",
+	"/nomatch/three",
+	"/nomatch/four",
+	"/nomatch/five",
+	"/nomatch/six",
+	"/nomatch/seven",
+	"/nomatch/eight",
+	"/nomatch/nine",
+	"/nomatch/ten",
+
+	// matching one:
+	"/one/nomatch/one",
+	"/one/nomatch/two",
+	"/one/nomatch/three",
+	"/one/nomatch/four",
+	"/one/nomatch/five",
+	"/one/nomatch/six",
+	"/one/nomatch/seven",
+	"/one/nomatch/eight",
+	"/one/nomatch/nine",
+	"/one/nomatch/ten",
+
+	// matching two:
+	"/one/two/nomatch/one",
+	"/one/two/nomatch/two",
+	"/one/two/nomatch/three",
+	"/one/two/nomatch/four",
+	"/one/two/nomatch/five",
+	"/one/two/nomatch/six",
+	"/one/two/nomatch/seven",
+	"/one/two/nomatch/eight",
+	"/one/two/nomatch/nine",
+	"/one/two/nomatch/ten",
+
+	// matching three:
+	"/one/two/three/nomatch/one",
+	"/one/two/three/nomatch/two",
+	"/one/two/three/nomatch/three",
+	"/one/two/three/nomatch/four",
+	"/one/two/three/nomatch/five",
+	"/one/two/three/nomatch/six",
+	"/one/two/three/nomatch/seven",
+	"/one/two/three/nomatch/eight",
+	"/one/two/three/nomatch/nine",
+	"/one/two/three/nomatch/ten",
+}
+
 func BenchmarkStaticTwo(b *testing.B) {
 	benchmarkWay(b, benchmarkData{
 		"GET", "/one/two",
@@ -249,6 +300,11 @@ func benchmarkWay(b *testing.B, d benchmarkData) {
 	r := NewRouter()
 	count := 0
 	var ctx context.Context
+	for _, pattern := range benchmarkDummyRoutes {
+		r.Handle(d.RouteMethod, pattern, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			b.Errorf("Routed to wrong method in benchmark!")
+		}))
+	}
 	r.Handle(d.RouteMethod, d.RoutePattern, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		count++
 		ctx = r.Context()
@@ -258,6 +314,7 @@ func benchmarkWay(b *testing.B, d benchmarkData) {
 		b.Errorf("NewRequest: %s", err)
 	}
 	w := httptest.NewRecorder()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		r.ServeHTTP(w, req)
 	}
